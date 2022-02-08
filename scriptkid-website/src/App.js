83d708096c5db5d3fab9bd2fa019eec8ba9e7e9a
ScriptkidHicks <tammashicks@gmail.com>
2022-02-08 15:07:17 -0800

    Starting to add routing to the project

--- scriptkid-website/src/App.js
+++ scriptkid-website/src/App.js
@@ -1,14 +1,28 @@
 import { BrowserRouter } from "react-router-dom";
-import styled from "styled-components"
+import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+
+import HeaderBar from "./components/HeaderBar";
+import FooterBar from "./components/FooterBar";
+
+import AboutPage from "./Pages/AboutPage";
+import LandingPage from "./Pages/LandingPage";
 
 function App() {
   return (
-    <Mainbody>hello</Mainbody>
+    <Mainbody>
+      <HeaderBar />
+      <Routes>
+        <Route path="/" exact element={<LandingPage />} />
+        <Route path="/About" element={<AboutPage />} />
+      </Routes>
+      <FooterBar />
+    </Mainbody>
   );
 }
 
 export default App;
 
-const Mainbody = styled.div `
-background-color: black;
+const Mainbody = styled.div`
+  background-color: black;
 `;
