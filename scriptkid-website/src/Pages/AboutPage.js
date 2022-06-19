e836405a271042dc967e2f355fc6577bfa051910
ScriptkidHicks <tammashicks@gmail.com>
2022-06-19 12:47:46 -0700

    some edits to the about page

--- scriptkid-website/src/Pages/AboutPage.js
+++ scriptkid-website/src/Pages/AboutPage.js
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-import { PageBody, NavLinks } from "../components/DefaultComponents";
+import { PageBody, NavLinks, TextLink } from "../components/DefaultComponents";
 
 function AboutPage() {
   return (
@@ -9,23 +10,30 @@ function AboutPage() {
         <PageTitle>About Me</PageTitle>
       </TitleAndBar>
       <AboutParagraph>
-        Currently I work for the University of Oregon Continuing Education
-        department as a front end development with a secondary role as a
-        designer. I'm finishing my degree in Computer Information Science this
-        coming Spring (June 2022), after which I'll be looking for work that
-        allows me to live in portland Oregon. I'm open to companies based out of
-        Portland, or remote work.
+        Till recently I worked for the University of Oregon Continuing Education
+        department as a front end development with some additional work doing
+        design. I completed my degree in computer information science with the
+        University of Oregon early June 2022, and am now in the search for work
+        either remote, or near Portland oregon.
         <br />
         <br />
         If you're looking for someone to do frontend development for you,
-        especially someone who has experience with react, and someone
-        comfortable working on back end projects, then I think I have something
-        to offer your company. I have a good eye for design and user interface,
-        as well as a solid understanding of software structure.
+        especially someone who has experience with react, then I think I have
+        something to offer your company. I have a good eye for design and user
+        interface, as well as a solid understanding of software structure.
+        Additionally, I have a experience working in backend, and in developing
+        an API.
         <br />
         <br />
-        Check out my projects on here, and if you like what you see, reach out
-        through my email for a resume or interview.
+        If you are a company that creates or aides in the playing of tabletop
+        roleplaying games, or if you create software that improves the quality
+        of patient care in the medical field, then I would love to hear from
+        you.
+        <br />
+        <br />
+        Check out my projects on{" "}
+        <TextLink text="here" endpoint="/ProjectsPage" />, and if you like what
+        you see, reach out through my email for an interview.
       </AboutParagraph>
       <NavLinks
         elements={[
